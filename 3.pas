63addb45cc3a7fb4ea1c76d690ef720b561058fd
Pavel-Shipitsyn <117993255+Pavel-Shipitsyn@users.noreply.github.com>
2022-12-14 13:12:52 +0300

    Add files via upload

---
+++ 3.pas
@@ -0,0 +1,14 @@
+﻿Program naconecto;
+uses graphabc;
+BEGIN
+setpencolor(clrandom);
+circle(50,50,10);
+circle(80,50,10);
+circle(110,50,10);
+circle(140,50,10);
+circle(170,50,10);
+circle(200,50,10);
+circle(230,50,10);
+circle(260,50,10);
+circle(290,50,10);
+END.
---
+++ 4.pas
@@ -0,0 +1,14 @@
+﻿uses graphabc;
+var xc,yc,r,i:integer;
+begin
+initgraphabc(xc,yc,'');
+xc:=getmaxX div 2;
+yc:=getmaxY div 2;
+r:=20;
+for i:=1 to 20 do
+ begin
+  circle(xc,yc,r);
+  r:=r+10
+ end;
+readln
+end.
